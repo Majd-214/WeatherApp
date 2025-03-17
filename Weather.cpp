18eb137ea0ea22021e055762fe1795791bbf08ab
Majd Aburas <aburasm@mcmaster.ca>
2025-03-17 15:50:41 -0400

    Initial commit

---
+++ Weather.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aburasm on 2025-03-17.
+//
+
+#include "Weather.h"
---
+++ Weather.h
@@ -0,0 +1,16 @@
+//
+// Created by aburasm on 2025-03-17.
+//
+
+#ifndef WEATHER_H
+#define WEATHER_H
+
+
+
+class Weather {
+
+};
+
+
+
+#endif //WEATHER_H
