3eb97a496ab8d36085a444618ea0c2c22a9706b1
Majd-214 <1aburasmaj@hdsb.ca>
2025-03-18 13:40:34 -0400

    Added Weather Class

---
+++ Weather.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Majd Aburas on 18-Mar-2025.
+//
+
+#include "Weather.h"
---
+++ Weather.h
@@ -0,0 +1,18 @@
+//
+// Created by Majd Aburas on 18-Mar-2025.
+//
+
+#ifndef WEATHERAPP_WEATHER_H_
+#define WEATHERAPP_WEATHER_H_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Weather {
+ private:
+  string test;
+};
+
+#endif //WEATHERAPP_WEATHER_H_
