e15c5bc03619ecfe1bb0261e66329f1e4322cc8c
Joshua <kamj4@mcmaster.ca>
2025-03-19 15:36:36 -0400

    temperature class

---
+++ Temperature.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Joshua Kam on 2025-03-19.
+//
+
+#include "Temperature.h"
---
+++ Temperature.h
@@ -0,0 +1,16 @@
+//
+// Created by Joshua Kam on 2025-03-19.
+//
+
+#ifndef TEMPERATURE_H
+#define TEMPERATURE_H
+
+
+
+class Temperature {
+
+};
+
+
+
+#endif //TEMPERATURE_H
