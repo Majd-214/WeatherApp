c04bb22f8bbf6608f28e55e553c8cc5f11a0404f
Majd-214 <1aburasmaj@hdsb.ca>
2025-03-30 16:34:21 -0400

    Added UI.h and Ui.cpp

---
+++ Ui.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Majdp on 30-Mar-2025.
+//
+
+#include "Ui.h"
---
+++ Ui.h
@@ -0,0 +1,16 @@
+//
+// Created by Majdp on 30-Mar-2025.
+//
+
+#ifndef UI_H
+#define UI_H
+
+
+
+class UI {
+
+};
+
+
+
+#endif //UI_H
